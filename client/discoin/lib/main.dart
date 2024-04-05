0f2a19cbc6e68434233e3a321649e84e7ab305bc
Michael <mixno20@gmail.com>
2024-04-05 20:40:59 +0800

    Попытка создать окно авторизации

--- client/discoin/lib/main.dart
+++ client/discoin/lib/main.dart
@@ -3,143 +3,106 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Discoin',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _loginController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool showPassword = false;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            container,
-          ],
+      appBar: AppBar(title: Text('Discoin', style: TextStyle(color: Colors.white),), backgroundColor: Colors.cyan,),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+              children: [
+                const SizedBox(height: 80),
+                const Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Авторизация',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w200, fontFamily: 'Montserrat'),
+                  ),
+                ),
+
+                Form(
+                  key: _formKey,
+                  child: Column(children: [
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _loginController,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          filled: true,
+                          hintText: 'Логин',
+                          hintStyle: const TextStyle(color: Colors.white),
+                          prefixIcon: const Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(36.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          fillColor: Colors.lightBlueAccent.shade200,
+                          focusColor: Colors.white),
+                    ),
+                    const SizedBox(height: 25),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: showPassword ? false : true,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          filled: true,
+                          hintText: 'Пароль',
+                          hintStyle: const TextStyle(color: Colors.white),
+                          prefixIcon: const Icon(
+                            Icons.security,
+                            color: Colors.white,
+                          ),
+                          suffixIcon: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  showPassword = !showPassword;
+                                });
+                              },
+                              child: Icon(
+                                  showPassword
+                                      ? Icons.visibility_off
+                                      : Icons.remove_red_eye,
+                                  color: Colors.white)),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(36.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          fillColor: Colors.lightBlueAccent.shade200,
+                          focusColor: Colors.white
+                      ),
+                    ),
+                  ]),
+                )
+              ]),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
-
-var container = Container(
-  color: Colors.purple,
-  width: 300.0,
-  height: 200.0,
-  margin: EdgeInsets.all(16.0),
-  child: Center(
-    child: Text(
-        'Hello app!',
-      style: TextStyle(
-          color: Colors.white,
-          fontSize: 30.0,
-          fontWeight: FontWeight.bold,
-      ),
-    ),
-  ),
-);
